d9396b89a55561a357c760fed82d9544d2fe420f
Enkhbilguun Nomch <bbg.1015@gmail.com>
2019-06-14 12:05:51 -0500

    finished the functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,26 +6,42 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(arg1, arg2, cb){
+  return cb(arg1,arg2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1,num2){
 
+  let sum = num1 + num2;
+  return sum;
+}
+
+function multiply(num1,num2){
+
+  let multiple = num1 * num2;
+  return multiple;
+}
+
+function greeting(string1,string2){
+
+  return `Hello ${string1} ${string2}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
-
-// Explanation: 
+// Explanation: Nested functions can inherit from its parent functions. If the nested function doesn't have the variables inside itself, it goes out to its parent function to find the variables.
 
 
 const external = "I'm outside the function";
